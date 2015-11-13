91b1db804a9eeada835fcfd5b43575d5b404690c
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-13 09:06:46 +0100

    Improved environment specific code

--- src/AlgoliaServiceProvider.php
+++ src/AlgoliaServiceProvider.php
@@ -13,7 +13,9 @@ namespace Vinkla\Algolia;
 
 use AlgoliaSearch\Client;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * This is the Algolia service provider class.
@@ -43,9 +45,9 @@ class AlgoliaServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/algolia.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('algolia.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('algolia');
         }
 
