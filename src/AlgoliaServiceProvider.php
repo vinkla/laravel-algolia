b41da0dfac960c2286b345818bb6b3a3951d7e5a
Vincent Klaiber <vincentklaiber@gmail.com>
2018-01-03 10:30:55 +0100

    Fixed config in phars

--- src/AlgoliaServiceProvider.php
+++ src/AlgoliaServiceProvider.php
@@ -43,7 +43,7 @@ class AlgoliaServiceProvider extends ServiceProvider
      */
     protected function setupConfig()
     {
-        $source = realpath(__DIR__.'/../config/algolia.php');
+        $source = realpath($raw = __DIR__.'/../config/algolia.php') ?: $raw;
 
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('algolia.php')]);
